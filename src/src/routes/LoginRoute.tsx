fbc56fb693b50eafcdaf7bb9a3b545b430264168
João Lucas <joaolucaspinheirotorres@gmail.com>
2023-05-07 16:21:04 -0300

    resolve conflicts with main

--- src/src/routes/LoginRoute.tsx
+++ src/src/routes/LoginRoute.tsx
@@ -3,6 +3,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { AppParamsList } from './ParamList';
 import { LoginPage } from '../pages/LoginPage';
 import { HomePage } from '../pages/HomePage';
+import { TestPage } from '../pages/TestPage';
+import ClientProfilePage from '../pages/ClientProfilePage';
+import ProfessionalProfilePage from '../pages/ProfessionalProfilePage';
 
 
 
@@ -16,6 +19,9 @@ function LoginRoute(): JSX.Element {
     <Stack.Navigator initialRouteName='Login'>
       <Stack.Screen name='Login' component={LoginPage} options={{ headerShown: false }} />
       <Stack.Screen name='Home' component={HomePage} options={{ headerShown: false }} />
+      <Stack.Screen name="Test" component={TestPage} />
+      <Stack.Screen name="ProfessionalProfile" component={ProfessionalProfilePage} />
+      <Stack.Screen name="ClientProfile" component={ClientProfilePage} />
     </Stack.Navigator>
 
   );
